b8012315495eca1c1c88fa56c5bf19a9995e2ff4
davidoog <david.goeta@hotmail.fr>
2024-09-19 15:34:54 +0200

    Update DB connection to JawsDB

--- db.php
+++ db.php
@@ -1,23 +1,11 @@
 <?php
-// Récupérer l'URL de la base de données depuis les variables d'environnement
-$url = getenv('JAWSDB_URL');
-
-if ($url) {
-    $dbparts = parse_url($url);
-
-    $host = $dbparts['host'];
-    $user = $dbparts['user'];
-    $pass = $dbparts['pass'];
-    $db = ltrim($dbparts['path'], '/');
-} else {
-    // Configuration pour l'environnement local
-    $host = 'localhost';
-    $db = 'arcadia_db';
-    $user = 'root';
-    $pass = '';
-}
-
+// Connexion à la base de données JawsDB sur Heroku
+$host = 'd6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$db = 'trt076z2um7x570f'; // nom de la base de données JawsDB
+$user = 'iagpxxf0qr3pb6i0'; // nom d'utilisateur JawsDB
+$pass = 'qtljsvffqzkgeymq'; // mot de passe JawsDB
 $charset = 'utf8mb4';
+
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -27,7 +15,7 @@ $options = [
 
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
