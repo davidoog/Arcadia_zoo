688de8408e8048989d4f7f23861495f391127eb0
davidoog <david.goeta@hotmail.fr>
2024-09-23 20:34:36 +0200

    amelioration responsive et menu

--- script.js
+++ script.js
@@ -24,6 +24,35 @@ const showAnimals = (habitat) => {
     }
 };
 
+// Ouvre/ferme le menu latéral en cliquant sur le menu hamburger
+document.getElementById('menu-icon').addEventListener('click', function(e) {
+    e.stopPropagation(); // Empêche la propagation du clic à d'autres éléments
+    const sideMenu = document.getElementById('side-menu');
+    
+    // Toggle pour afficher ou cacher le menu
+    if (sideMenu.classList.contains('open')) {
+        sideMenu.classList.remove('open');
+        sideMenu.classList.add('closing'); // Ajouter l'animation pour fermer vers la gauche
+    } else {
+        sideMenu.classList.remove('closing');
+        sideMenu.classList.add('open'); // Ajouter l'animation pour ouvrir depuis la gauche
+    }
+});
+
+// Ferme le menu si on clique en dehors du menu ou du menu icon
+document.addEventListener('click', function(event) {
+    const sideMenu = document.getElementById('side-menu');
+    const menuIcon = document.getElementById('menu-icon');
+
+    // Vérifie si l'endroit cliqué est en dehors du menu ou du bouton
+    if (!sideMenu.contains(event.target) && !menuIcon.contains(event.target)) {
+        if (sideMenu.classList.contains('open')) {
+            sideMenu.classList.remove('open'); // Ferme le menu
+            sideMenu.classList.add('closing'); // Animer pour fermeture vers la gauche
+        }
+    }
+});
+
 // Fonction pour afficher les détails d'un animal spécifique
 function showAnimalDetails(animal) {
     // Cacher tous les détails d'animaux
