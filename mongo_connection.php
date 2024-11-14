730306a3bb9ea97645e4f4ae323aa85dae39a24a
davidoog <david.goeta@hotmail.fr>
2024-11-14 15:16:40 +0100

    maj bdd mongodb

--- mongo_connection.php
+++ mongo_connection.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php'; // Inclure Composer autoload
 
 // Connexion à MongoDB
 try {
-    $client = new MongoDB\Client("mongodb://localhost:27017");
+    $client = new MongoDB\Client("mongodb://admin:147123C1secret!@localhost:27017/?authSource=admin");
     $db = $client->Zoo_Arcadia; // Remplace par le nom de ta base
     $collection = $db->Animals_visits; // Nom de ta collection
 } catch (Exception $e) {
