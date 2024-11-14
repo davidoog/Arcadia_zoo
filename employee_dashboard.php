88b1d0e482cca8d43feab47bd0b533f6b17306d3
davidoog <david.goeta@hotmail.fr>
2024-11-14 16:00:26 +0100

    maj erreur css employee dashboard

--- employee_dashboard.php
+++ employee_dashboard.php
@@ -67,7 +67,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de bord Employé - Zoo Arcadia</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="employee_dashboard.css">
+    
 
     <style>
         .employee-section {
