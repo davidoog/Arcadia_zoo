f8a880f9d62c92359a6c651bdf6468164c6ac451
davidoog <david.goeta@hotmail.fr>
2024-11-17 16:43:02 +0100

    tentative de bug sur un formulaire de manage_services.php

--- manage_services.js
+++ manage_services.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
     const serviceForm = document.getElementById("manageServiceForm");
 
     // Vérifie si le formulaire existe avant d'ajouter l'événement
